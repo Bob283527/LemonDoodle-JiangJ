0d0ab03121d036cd709d86edcf8c5597f0a4e580
Bob283527 <jonjiang27@7hills.org>
2026-01-20 08:50:32 -0500

    Testing code for cups and maybe adding detail on the stand will probably change the design of the stand and then I will make a background

--- LemonadeStand.java
+++ LemonadeStand.java
@@ -14,20 +14,26 @@ public class LemonadeStand {
 		
 		Rectangle roofThing = new Rectangle(650, 525, 600, 25);
         
-          Path p1 = new Path();       // 1) Create an empty Path object
+        Path p1 = new Path();       // 1) Create an empty Path object
         p1.moveTo(650.0,  525.0);    // 2) Move the virtual pencil to (150, 50)
         p1.lineTo(700.0, 475.0);    // 3) Draw a line down to (150, 150)
         p1.lineTo( 1250.0, 525.0);    // 4) Add a line over to (50, 150)
         p1.lineTo( 1175.0, 475.0);    // 5) Add a line up to (50, 50)
         p1.lineTo(650.0, 525.0);
 
-		Path p3 = new Path();       // 1) Create an empty Path object
-        p3.moveTo( 700.0,  700.0);   // 2) Move your pencil to (150, 50)
-        p3.lineTo( 700.0, 650.0);   // 3) Draw a line down to (150, 150)
-                                    // 4) Add a bezier spline over to (50, 150)
-        p3.curveTo(100.0, 200.0, 100.0, 100.0, 1175.0, 650.0);
-        p3.lineTo(  1175.0,  700.0);   // 5) Add a line up to (50, 50)
-        p3.closePath();             // 6) Close the Path.
+		/*Path p3 = new Path();       // 1) Create an empty Path object
+        p3.moveTo(650.0, 475.0);   // 2) Move your pencil to (150, 50)
+		p3.lineTo(1175.0, 475.0);
+		*/
+		
+		//testing for cups
+		Path p2 = new Path();       // 1) Create an empty Path object
+        p2.moveTo(150.0,  50.0);    // 2) Move your pencil to (150, 50)
+        p2.lineTo(150.0, 150.0);    // 3) Draw a line down to (150, 150)
+                                    // 4) Add a quadratic segment over to (50, 150)
+        p2.quadTo(100.0, 200.0, 50.0, 150.0);
+        p2.lineTo( 50.0,  50.0);    // 5) Add a line up to (50, 50)
+        p2.closePath();             // 6) Close the Path.
 	}
 	
 	public static void main(String[] args) {
